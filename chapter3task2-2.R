b6225f676e91eff84c45d0d81aa69ead79590a2f
ahedfg <74929731+ahedfg@users.noreply.github.com>
2020-12-03 00:00:20 +0100

    asd

--- chapter3task2-2.R
+++ chapter3task2-2.R
@@ -5,7 +5,6 @@ library(jsonlite)  # converts JSON files to R objects
 library(glue)      # concatenate strings
 library(stringi)
 url_home          <- "https://www.rosebikes.de/fahrr%C3%A4der"
-xopen(url_home) # Open links directly from RStudio to inspect them
 
 # Read in the HTML for the entire webpage
 html_home         <- read_html(url_home)
@@ -35,7 +34,6 @@ family_id_css <- bike_family_tbl %>%
   stringr::str_c(collapse = ", ")
 
 bike_family_tbl
-## "#js-navigationList-ROAD, #js-navigationList-MOUNTAIN, #js-navigationList-EBIKES, #js-navigationList-HYBRID-CITY, #js-navigationList-YOUNGHEROES"
 
 
 
@@ -60,89 +58,6 @@ bike_category_tbl <- html_home %>%
   # Some categories are listed multiple times.
   # We only need unique values
   distinct(url)
-
-bike_category_tbl
-
-
-
-# # 2.0 COLLECT BIKE DATA ----
-# 
-# # 2.1 Get URL for each bike of the Product categories
-# 
-# # select first bike category url
-# bike_category_url <- bike_category_tbl$url[1]
-# 
-# # Alternatives for selecting values
-# # bike_category_url <- bike_category_tbl %$% url %>% .[1]
-# # bike_category_url <- bike_category_tbl %>% pull(url) %>% .[1]
-# # bike_category_url <- deframe(bike_category_tbl[1,])
-# # bike_category_url <- bike_category_tbl %>% first %>% first
-# 
-# xopen(bike_category_url)
-# 
-# html_bike_category  <- read_html(bike_category_url)
-# bike_url_tbl        <- html_bike_category %>%
-#   
-#   # Get the 'a' nodes, which are hierarchally underneath 
-#   # the class productTile__contentWrapper
-#   html_nodes(css = ".productTile__contentWrapper > a") %>%
-#   html_attr("href") %>%
-#   
-#   # Remove the query parameters of the URL (everything after the '?')
-#   str_remove(pattern = "\\?.*") %>%
-#   
-#   # Convert vector to tibble
-#   enframe(name = "position", value = "url")
-# 
-# # 2.1.2 Extract the descriptions (since we have retrieved the data already)
-# bike_desc_tbl <- html_bike_category %>%
-#   
-#   # Get the nodes in the meta tag where the attribute itemprop equals description
-#   html_nodes('.productTile__productSummaryLeft > meta[itemprop="description"]') %>%
-#   
-#   # Extract the content of the attribute content
-#   html_attr("content") %>%
-#   
-#   # Convert vector to tibble
-#   enframe(name = "position", value = "description")
-# 
-# # 2.1.3 Get even more data from JSON files
-# bike_json_tbl  <- html_bike_category %>%
-#   
-#   html_nodes(css = '.productGrid__listItem.xlt-producttile > div') %>%
-#   html_attr("data-gtm-impression") %>%
-#   
-#   # Convert the JSON format to dataframe
-#   # map runs that function on each element of the list
-#   map(fromJSON) %>% # need JSON ### need lists
-#   
-#   # Extract relevant information of the nested list
-#   map(purrr::pluck, 2, "impressions") %>% # Need purrr and expl above
-#   
-#   # Set "not defined" and emtpy fields to NA (will be easier to work with)
-#   map(na_if, "not defined") %>%
-#   map(na_if, "") %>%
-#   
-#   # The class of dimension56 and price varies between numeric and char.
-#   # This converts this column in each list to numeric
-#   # across allows to perform the same operation on multiple columns
-#   map(~mutate(., across(c("dimension56","price"), as.numeric))) %>%
-#   
-#   # Stack all lists together
-#   bind_rows() %>%
-#   # Convert to tibble so that we have the same data format
-#   as_tibble() %>%
-#   
-#   # Add consecutive numbers so that we can bind all data together
-#   # You could have also just use bind_cols()
-#   rowid_to_column(var='position') %>%
-#   left_join(bike_desc_tbl) %>%
-#   left_join(bike_url_tbl)
-
-
-
-
-
 
 
 get_bike_data <- function(url) {
@@ -190,6 +105,8 @@ for (i in seq_along(bike_category_tbl$url)) {
   print(i)
   
 }
+bike_data_tbl <- bike_data_tbl %>%
+  rename("model" = "name")
 bike_data_tbl
 saveRDS(bike_data_tbl, "rosebikes_data_tbl.rds")
 
